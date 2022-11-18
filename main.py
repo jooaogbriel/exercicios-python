cd50ac80dc94c0c2d71d3d61042530e5c4100d08
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-11-18 14:54:54 -0300

    ex003

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name = input('Qual é seu nome?')
-print(f'boas vindas {name}')
+y = int(input('digite um numero '))
+x = int(input('digite outro numero '))
+s = y + x
+print(f'a soma de {y} + {x} é {s}')
